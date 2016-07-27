160f3cbb5f96eff5fb8b73624228ceb7c8208b71
David Mansfield <david@orthanc.cobite.com>
2016-07-27 10:48:53 -0400

    Change the hardcoded path in the test client

--- parsey_client/index.js
+++ parsey_client/index.js
@@ -2,7 +2,7 @@ var grpc = require('grpc');
 
 var protoDescriptor = grpc.load({root: __dirname+'/api', file:'cali/nlp/parsey_api.proto'});
 
-var service = new protoDescriptor.cali.nlp.ParseyService("172.17.0.2:9000", grpc.credentials.createInsecure());
+var service = new protoDescriptor.cali.nlp.ParseyService("127.0.0.1:9001", grpc.credentials.createInsecure());
 
 service.parse(["This is the first sentence", "I love this sentence"], function(err, response) {
 	console.log(JSON.stringify(response,null,'  '));
